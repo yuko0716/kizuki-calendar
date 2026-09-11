fb24250ca263f364c5b564b7a656426a43f4e90c
yuko0716 <yuko.sato.0716@gmail.com>
2026-09-11 17:13:13 +0900

    Update storage test for easy-language copy

--- test/storage.test.js
+++ test/storage.test.js
@@ -28,5 +28,5 @@ test("readBackup rejects malformed and oversized photo data", async () => {
     version: 2,
     records: { "2026-09-10": { answers: [{}, {}, {}], photo: "x".repeat(MAX_PHOTO_DATA_URL_LENGTH + 1) } },
   }) };
-  await assert.rejects(() => readBackup(file), /読み込めません/);
+  await assert.rejects(() => readBackup(file), /読みこめません/);
 });
